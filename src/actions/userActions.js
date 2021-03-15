a7b52b906d744377d0480a6ac5b2399bc6d5cccb
fouad magdy <fouad_magdy@hotmail.com>
2021-03-15 16:44:46 +0200

    logout

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,4 +1,5 @@
-import { USER_LIST_REQUEST, USER_LIST_FAIL, USER_LIST_RESET, USER_LIST_SUCCESS, USER_LOGIN_SUCCESS, USER_LOGIN_REQUEST, USER_LOGIN_FAIL } from '../constants/userConstants'
+import { QUESTION_LIST_RESET } from '../constants/questionConstants'
+import { USER_LIST_REQUEST, USER_LIST_FAIL, USER_LIST_RESET, USER_LIST_SUCCESS, USER_LOGIN_SUCCESS, USER_LOGIN_REQUEST, USER_LOGIN_FAIL, USER_LOGOUT } from '../constants/userConstants'
 
 import {
     _getUsers,
@@ -17,7 +18,28 @@ export const login = (data) => async (dispatch) => {
             payload: data,
         })
 
-        //   localStorage.setItem('userInfo', JSON.stringify(data))
+    } catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload: error
+        })
+    }
+}
+
+export const logout = () => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGOUT,
+        })
+
+        dispatch({
+            type: USER_LIST_RESET,
+        })
+
+        dispatch({
+            type: QUESTION_LIST_RESET,
+        })
+
     } catch (error) {
         dispatch({
             type: USER_LOGIN_FAIL,
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,38 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav } from 'react-bootstrap'
 
+import { logout } from '../actions/userActions'
+
 const Header = () => {
+
+    const dispatch = useDispatch()
 
     const userLogin = useSelector((state) => state.userLogin)
     const { userlogin } = userLogin
 
     const userList = useSelector((state) => state.userList)
-    const { loading, error, users } = userList
+    const { users } = userList
+
+    const handleLogout = () => {
+        dispatch(logout())
+    }
 
     return (
         <Navbar bg="light" expand="lg">
-            <LinkContainer to='/'>
-                <Navbar.Brand>Would you Rather</Navbar.Brand>
-            </LinkContainer>
+            <Navbar.Brand>Would you Rather</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
                     <Nav.Link href="#home">New Question</Nav.Link>
                     <Nav.Link href="#link">LeaderBoard</Nav.Link>
                     <Nav.Link href="#link"><img src={users && users[userlogin].avatarURL} className="img-fluid" style={{ width: '30px' }} alt="avatar" /> {userlogin}</Nav.Link>
-                    <Nav.Link href="#link">Logout</Nav.Link>
+
+                    <LinkContainer to='/'>
+                        <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                    </LinkContainer>
+
                 </Nav>
 
             </Navbar.Collapse>
